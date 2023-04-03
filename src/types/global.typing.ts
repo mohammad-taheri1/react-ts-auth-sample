3b49e81128723508dfcdebe29222ef0f88035098
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-04-04 01:04:34 +0430

    update types

--- src/types/global.typing.ts
+++ src/types/global.typing.ts
@@ -2,3 +2,24 @@ export interface ILoginData {
   email: string;
   password: string;
 }
+export interface IUser {
+  id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+export interface IUsersResponse {
+  page: string;
+  per_page: string;
+  total: string;
+  total_pages: string;
+  data: IUser[];
+  support: any;
+}
+
+export interface IUserDetailsResponse {
+  data: IUser;
+  support: any;
+}
